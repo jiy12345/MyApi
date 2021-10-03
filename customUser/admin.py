4f1dcc9ee45c5fd5ef7a174984c8b3c8ae8b2470
jiy12345 <jiy12367@gmail.com>
2021-10-04 03:45:56 +0900

    모델 만들고 해당 모델이 admin 패널에 출력되도록 하기

--- customUser/admin.py
+++ customUser/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CarSpecs # admin 패널에서 이용하기 위해 모델 가져오기
 
 # Register your models here.
+admin.site.register(CarSpecs) # admin 패널에 등록
+
+
--- customUser/models.py
+++ customUser/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# 차 스펙 갖는 모델 작성해보기
+class CarSpecs(models.Model):
+    car_brand = models.Charfield(max_length=50)
+    car_model = models.CharField(max_length=100)
+    production_year = models.CharField(max_length=10)
+    car_body = models.CharField(max_length=50)
+    engine_type = models.CharField(max_length=50)
+
+    # 출력될 데이터 결정하기!
+    def __str__(self):
+        return self.car_brand
